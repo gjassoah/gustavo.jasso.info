2c966dc92dfd18ee3b840831475ea8704746735d
Gustavo Jasso <gustavo.jasso@math.lu.se>
2023-08-31 19:43:56 +0200

    delete deploy.sh

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-hugo &&
-echo "Building tarball..."
-tar -C public -cvz . > site.tar.gz &&
-echo "Publishing website to sourcehut..."
-hut pages publish -d gustavo.jasso.info site.tar.gz
